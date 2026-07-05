c903762254a1f4e1c5a0ea14955e6ab1e855ebe4
hnc450 <henoctumonakiese@gmail.com>
2026-07-05 20:54:46 +0100

    fix(server-error): Resolve error 500

---
+++ database/migrations/admin.sql
@@ -0,0 +1,6 @@
+USE zfinance;
+
+INSERT IGNORE INTO admins(`name`,`email`,`pass`) 
+  VALUES 
+  ("henock","henoctumonakiese@gmail.com","$2y$12$MT71gobKQKU49BGTe9pkaexo6I7qVcFG0fFdQb696Xb4fCW20/MZS"),
+  ("Tshibaak","zaimon0123456789@gmail.com","$2y$12$fcB3iQIhqPgyiKo1WgvKDu7Y/lV/yWrpTQtCRjf.6gOG/uamuDrva");
